d8f03d5024d5782a345f80689e4fab6a67e19a68
turbiani <leo.turbiani@gmail.com>
2016-06-20 20:59:27 -0300

    Primeiro Hello World com DataBind

--- app/src/main/java/dominando/addicted/android/databinding/MainActivity.java
+++ app/src/main/java/dominando/addicted/android/databinding/MainActivity.java
@@ -1,13 +1,20 @@
 package dominando.addicted.android.databinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import dominando.addicted.android.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Usuario usuario = new Usuario("Leo", "Turbiabi");
+        binding.setUsuario(usuario);
     }
 }
---
+++ app/src/main/java/dominando/addicted/android/databinding/Usuario.java
@@ -0,0 +1,14 @@
+package dominando.addicted.android.databinding;
+
+/**
+ * Created by turbiani on 20/06/16.
+ */
+public class Usuario {
+    public String nome;
+    public String sobrenome;
+
+    public Usuario(String nome, String sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
