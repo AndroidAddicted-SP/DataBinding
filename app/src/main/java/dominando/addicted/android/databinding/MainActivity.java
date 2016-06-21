7eb2e35c827f72f9381033fd193c244b5d09acb2
turbiani <leo.turbiani@gmail.com>
2016-06-20 21:42:15 -0300

    Inserindo Listener que avisa a View quando o Model é alterado

--- app/src/main/java/dominando/addicted/android/databinding/MainActivity.java
+++ app/src/main/java/dominando/addicted/android/databinding/MainActivity.java
@@ -3,10 +3,14 @@ package dominando.addicted.android.databinding;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import dominando.addicted.android.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +18,44 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        Usuario usuario = new Usuario("Leo", "Turbiabi");
+        usuario = new Usuario("Leo", "Turbiani");
         binding.setUsuario(usuario);
         binding.setTratador(new TratadorMagico());
+
+        EditText edtNome = (EditText) findViewById(R.id.edtNome);
+        edtNome.addTextChangedListener(new AoMudarTexto(R.id.edtNome));
+
+        EditText edtSobreNome = (EditText) findViewById(R.id.edtSobreNome);
+        edtSobreNome.addTextChangedListener(new AoMudarTexto(R.id.edtSobreNome));
+    }
+
+    class AoMudarTexto implements TextWatcher{
+        private int id;
+
+        public AoMudarTexto(int id){
+            this.id = id;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            switch (id){
+                case R.id.edtNome:
+                    usuario.setNome(charSequence.toString());
+                    break;
+                case R.id.edtSobreNome:
+                    usuario.setSobrenome(charSequence.toString());
+                    break;
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
     }
 }
--- app/src/main/java/dominando/addicted/android/databinding/Usuario.java
+++ app/src/main/java/dominando/addicted/android/databinding/Usuario.java
@@ -1,14 +1,41 @@
 package dominando.addicted.android.databinding;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by turbiani on 20/06/16.
  */
-public class Usuario {
-    public String nome;
-    public String sobrenome;
+public class Usuario extends BaseObservable {
+    private String nome;
+    private String sobrenome;
 
     public Usuario(String nome, String sobrenome){
         this.nome = nome;
         this.sobrenome = sobrenome;
+    }
+
+    @Bindable
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+        notifyPropertyChanged(
+            dominando.addicted.android.databinding.BR.nome
+        );
+    }
+
+    @Bindable
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+        notifyPropertyChanged(
+                dominando.addicted.android.databinding.BR.sobrenome
+        );
     }
 }
